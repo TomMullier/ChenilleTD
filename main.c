3bd538c0793dd71c49f01c0a9f93bdc83f151f90
Tom Mullier <tom.mullier@outlook.fr>
2020-12-01 13:48:42 +0100

    red apple

--- main.c
+++ main.c
@@ -31,6 +31,7 @@ int main() {
     // Set console
     openConsole();
     setBackGroundColor(15);
+    hideCursor();
     setWriteColor(1);
     for (int i = 0; i < TAILLE_FEUILLE_X; i++) {
         for (int j = 0; j < TAILLE_FEUILLE_Y; j++) {
@@ -42,7 +43,9 @@ int main() {
     // Set '@'
     for (int i = 0; i < 9; i++) {
         moveCursor(rangedRand(0, 30), rangedRand(0, 30));
+        setWriteColor(4);
         plotChar('@');
+        setWriteColor(1);
     }
     moveCursor(29, 29);
     printf(" ");
@@ -80,83 +83,4 @@ int main() {
     Sleep(100000000000);
     closeConsole();
     return EXIT_SUCCESS;
-}
-
-void moveTo(Snake* snake, COORD head) {
-    COORD to_erase;
-    to_erase.X = snake->coordinates[snake->length - 1].X;
-    to_erase.Y = snake->coordinates[snake->length - 1].Y;
-    for (int i = snake->length - 1; i >= 0; i--) {
-        snake->coordinates[i] = (i != 0) ? snake->coordinates[i - 1] : head;
-    }
-    // Remove the snake queue
-    moveCursor(to_erase.X, to_erase.Y);
-    plotChar(' ');
-    // Update the snake head
-    moveCursor(snake->coordinates[0].X, snake->coordinates[0].Y);
-    plotChar('X');
-    moveCursor(snake->coordinates[1].X, snake->coordinates[1].Y);
-    plotChar('0');
-}
-
-// Choose a random direction between (N)orth, (E)ast, (S)outh and (W)est
-// Doesn't return the 'last_direction'
-// Doesn't return the opposite of 'last_direction' to avoid backwards
-// Example : getRandomDirection('N') doesn't return 'N' or 'S'
-char getRandomDirection(char last_direction) {
-    char r;
-    char opposite;
-    int nb = rangedRand(0, 4);
-    switch (nb) {
-    case 0:
-        r = 'N';
-        opposite = 'S';
-        break;
-    case 1:
-        r = 'E';
-        opposite = 'W';
-        break;
-    case 2:
-        r = 'S';
-        opposite = 'N';
-        break;
-    case 3:
-        r = 'W';
-        opposite = 'E';
-        break;
-    default:
-        r = 'N';
-        opposite = 'S';
-        break;
-    }
-    if (r == last_direction || opposite == last_direction) r = getRandomDirection(last_direction);
-    return r;
-}
-
-// Calcul the next coordinates with the chosen direction and the current position
-COORD getDestination(COORD position, char direction) {
-    COORD destination;
-    switch (direction) {
-    case 'N':
-        destination.X = position.X;
-        destination.Y = position.Y + 1;
-        break;
-    case 'E':
-        destination.X = position.X + 1;
-        destination.Y = position.Y;
-        break;
-    case 'S':
-        destination.X = position.X;
-        destination.Y = position.Y - 1;
-        break;
-    case 'W':
-        destination.X = position.X - 1;
-        destination.Y = position.Y;
-        break;
-    default:
-        destination.X = position.X;
-        destination.Y = position.Y + 1;
-        break;
-    }
-    return destination;
 }
--- move.h
+++ move.h
@@ -15,95 +15,85 @@ void logSnake(COORD a[], int size) {
 		printf("%d: %d;%d\n", i, a[i].X, a[i].Y);
 	}
 }
-/*
-void direction(COORD* snake);
-void goUp(COORD* snake);
-void goDown(COORD* snake);
-void goRight(COORD* snake);
-void goLeft(COORD* snake);
 
-// Random Fruits
-void remplissage() {
-	for (int i = 0; i < 9; i++)
-	{
-		moveCursor(rangedRand(0, 30), rangedRand(0, 30));
-		plotChar('@');
-	}
-	moveCursor(29, 29);
-	printf(" ");
-}
 
-void updateSnake(COORD* snake, COORD head) {
-	COORD to_erase;
-	to_erase.X = snake[LENGTH - 1].X;
-	to_erase.Y = snake[LENGTH - 1].Y;
-	for (int i = LENGTH - 1; i >= 0; i--) {
-		snake[i] = (i != 0) ? snake[i - 1] : head;
-	}
-	// Remove the snake queue
-	moveCursor(to_erase.X, to_erase.Y);
-	plotChar(' ');
-	// Update snake into the console
-	for (int i = LENGTH - 1; i >= 0; i--) {
-		int x = snake[i].X;
-		int y = snake[i].Y;
-		moveCursor(x, y);
-		plotChar(i == 0 ? 'X' : '0');
-	}
+void moveTo(Snake* snake, COORD head) {
+    hideCursor();
+    COORD to_erase;
+    to_erase.X = snake->coordinates[snake->length - 1].X;
+    to_erase.Y = snake->coordinates[snake->length - 1].Y;
+    for (int i = snake->length - 1; i >= 0; i--) {
+        snake->coordinates[i] = (i != 0) ? snake->coordinates[i - 1] : head;
+    }
+    // Remove the snake queue
+    moveCursor(to_erase.X, to_erase.Y);
+    plotChar(' ');
+    // Update the snake head
+    moveCursor(snake->coordinates[0].X, snake->coordinates[0].Y);
+    plotChar('X');
+    moveCursor(snake->coordinates[1].X, snake->coordinates[1].Y);
+    plotChar('0');
 }
 
-void direction(COORD* snake) {
-	int tempnumber = rangedRand(1, 5);
-	switch (tempnumber)
-	{
-	case 1:
-		goUp(snake);
-		break;
-	case 2:
-		goRight(snake);
-		break;
-	case 3:
-		goDown(snake);
-		break;
-	case 4:
-		goLeft(snake);
-		break;
-	default:
-		direction(snake);
-		break;
-	}
+// Choose a random direction between (N)orth, (E)ast, (S)outh and (W)est
+// Doesn't return the 'last_direction'
+// Doesn't return the opposite of 'last_direction' to avoid backwards
+// Example : getRandomDirection('N') doesn't return 'N' or 'S'
+char getRandomDirection(char last_direction) {
+    char r;
+    char opposite;
+    int nb = rangedRand(0, 4);
+    switch (nb) {
+    case 0:
+        r = 'N';
+        opposite = 'S';
+        break;
+    case 1:
+        r = 'E';
+        opposite = 'W';
+        break;
+    case 2:
+        r = 'S';
+        opposite = 'N';
+        break;
+    case 3:
+        r = 'W';
+        opposite = 'E';
+        break;
+    default:
+        r = 'N';
+        opposite = 'S';
+        break;
+    }
+    if (r == last_direction || opposite == last_direction) r = getRandomDirection(last_direction);
+    return r;
 }
 
-void goUp(COORD* snake) {
-	COORD new_head;
-	new_head.X = snake[0].X;
-	new_head.Y = snake[0].Y - 1;
-	updateSnake(snake, new_head);
-	Sleep(500);
-	hideCursor();
+// Calcul the next coordinates with the chosen direction and the current position
+COORD getDestination(COORD position, char direction) {
+    COORD destination;
+    hideCursor();
+    switch (direction) {
+    case 'N':
+        destination.X = position.X;
+        destination.Y = position.Y + 1;
+        break;
+    case 'E':
+        destination.X = position.X + 1;
+        destination.Y = position.Y;
+        break;
+    case 'S':
+        destination.X = position.X;
+        destination.Y = position.Y - 1;
+        break;
+    case 'W':
+        destination.X = position.X - 1;
+        destination.Y = position.Y;
+        break;
+    default:
+        destination.X = position.X;
+        destination.Y = position.Y + 1;
+        break;
+    }
+    return destination;
 }
-void goRight(COORD* snake) {
-	COORD new_head;
-	new_head.X = snake[0].X + 1;
-	new_head.Y = snake[0].Y;
-	updateSnake(snake, new_head);
-	Sleep(500);
-	hideCursor();
-}
-void goDown(COORD* snake) {
-	COORD new_head;
-	new_head.X = snake[0].X;
-	new_head.Y = snake[0].Y + 1;
-	updateSnake(snake, new_head);
-	Sleep(500);
-	hideCursor();
-}
-void goLeft(COORD* snake) {
-	COORD new_head;
-	new_head.X = snake[0].X - 1;
-	new_head.Y = snake[0].Y;
-	updateSnake(snake, new_head);
-	Sleep(500);
-	hideCursor();
-	
-}*/
